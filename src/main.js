8b5ba1ece14b076cca29e2ef4f88f1806c87fa98
陶庆澳 <1101389964@qq.com>
2021-04-10 15:46:35 +0800

    完成商品列表开发

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ import "./assets/fonts/iconfont.css";
 /* 配置数据请求路径 */
 import axios from "axios";
 import TreeTable from "vue-table-with-tree-grid";
+import VueQuillEditor from "vue-quill-editor";
+import "quill/dist/quill.core.css"; // import styles
+import "quill/dist/quill.snow.css"; // for snow theme
+import "quill/dist/quill.bubble.css"; // for bubble theme
 
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
 axios.interceptors.request.use((config) => {
@@ -20,6 +24,7 @@ Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 
 Vue.component("tree-table", TreeTable);
+Vue.use(VueQuillEditor);
 
 /* 过滤器 */
 Vue.filter("dateFormat", function(originVal) {
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -34,6 +34,11 @@ import {
   Tabs,
   TabPane,
   Timeline,
+  Steps,
+  Step,
+  Checkbox,
+  CheckboxGroup,
+  Upload,
 } from "element-ui";
 
 Vue.use(Button);
@@ -67,6 +72,11 @@ Vue.use(Alert);
 Vue.use(Tabs);
 Vue.use(TabPane);
 Vue.use(Timeline);
+Vue.use(Steps);
+Vue.use(Step);
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
+Vue.use(Upload);
 
 Vue.prototype.$message = Message;
 Vue.prototype.$confirm = MessageBox.confirm;
